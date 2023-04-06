7aaed9bca336edd32707691a187e7fe814af8ab6
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-04-06 08:31:51 +0100

    fourth commit

--- client/script.js
+++ client/script.js
@@ -1,8 +1,15 @@
-import { getItems } from "./api"
+import { getItems } from "./api" 
+
+const itemTemplate = document.getElementById('item-template')
+const itemList = document.querySelector('[data-item-list]')
 
 async function loadItems() {
     const items = await getItems()
-    console.log(items)
+
+    itemList.innerHtml = ''
+    items.forEach(item => {
+        const itemElement = itemTemplate.content.cloneNode(true)
+    })
 }
 
 loadItems()
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const items = require('./items.json')
+
+app.use(cors({
+    origin: "http://localhost:1234",
+}))
 
 app.get('/items', (req, res) => {
     res.json(items.map(item => {
